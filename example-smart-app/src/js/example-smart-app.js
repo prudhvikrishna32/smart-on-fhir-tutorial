88af62936b58bccaa767f16a2784b0e68889d4b7
prudhvikrishna32 <prudhvikrishna320@gmail.com>
2026-04-29 13:45:28 -0400

    Add alert message for debugging

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -43,7 +43,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          alert()
+          alert('you are here')
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
