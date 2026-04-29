0e7371bff44e974d80ff335505abca2f6ec857d3
prudhvikrishna32 <prudhvikrishna320@gmail.com>
2026-04-29 14:49:43 -0400

    Fix case sensitivity in byCodes function call

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -44,7 +44,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          var temperature = bycodes('8310-5');
+          var temperature = byCodes('8310-5');
   
           alert('you are here');
           var p = defaultPatient();
