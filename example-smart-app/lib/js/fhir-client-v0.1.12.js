00d8604ab4140b8423e4217a8039b1ff1ee9766d
prudhvikrishna32 <prudhvikrishna320@gmail.com>
2026-04-29 12:47:03 -0400

    Fix token expiration check logic

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -17189,7 +17189,6 @@ BBClient.ready = function(input, callback, errback){
       var tokenResponse = getPreviousToken();
       var payloadCheck = jwt.decode(tokenResponse.access_token);
       var nearExpTime = Math.floor(Date.now() / 1000) >= (tokenResponse.expires_in - 120);
-
       if (tokenResponse.refresh_token
         && tokenResponse.scope.indexOf('online_access') > -1
         && nearExpTime) { // refresh token flow
